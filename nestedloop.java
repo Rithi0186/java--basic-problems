61b8a4632a60bf49cc05069ee390db28fdc934fb
Rithi0186 <rithisubhasasi123@gmail.com>
2025-08-27 14:52:29 +0530

    Create nestedloop.java

---
+++ nestedloop.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int dollarsToSpareInPocketToday = 15;
+
+        if( dollarsToSpareInPocketToday >= 15 ) 
+        {
+            System.out.println("I will go to the Movies");
+        } else 
+        {
+            if(dollarsToSpareInPocketToday >= 3) 
+            {
+                System.out.println("I will go to a Cafe for a cup of coffee.");
+            } else 
+            {
+                System.out.println("I will chill at home.");
+            }
+        }
+    }
+}
